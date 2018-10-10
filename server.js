4322aafb6dfd34861289a64cfec7f4fa582ba44e
E. Scott <erics@salemtube.com>
2018-10-10 12:27:19 -0400

    added projects page

--- server.js
+++ server.js
@@ -56,4 +56,10 @@ app.get("/bad", (req, res) => {
   });
 });
 
+app.get("/projects", (req, res) => {
+  res.render("projects.hbs", {
+    pageTitle: "Projects home"
+  });
+});
+
 app.listen(port);
